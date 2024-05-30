b52df39fa47bfed9eede41133ec911ed6a50ca03
dblanque <dylan.blanque@gmail.com>
2024-05-30 17:42:29 -0300

    Add y/n input function

---
+++ py_pve_toolkit/utils/yes_no_input.py
@@ -0,0 +1,25 @@
+DEFAULT_CHOICES = {
+	"yes":["yes","y"],
+	"no":["no","n"]
+}
+def yes_no_input(
+		msg: str,
+		input_default=None,
+		input_choices=DEFAULT_CHOICES
+	):
+	choices_str = ' | '.join(input_choices)
+	if input_default:
+		default_str = f" [{input_default.upper()}]"
+	else: default_str = ""
+	while True:
+		r = input(f"{msg} {choices_str}{default_str}:")
+		if r.lower() in input_choices["yes"]:
+			return True
+		elif r.lower() in input_choices["no"]:
+			return False
+		elif input_default == True or input_default in input_choices["yes"]:
+			return True
+		elif input_default == False or input_default in input_choices["no"]:
+			return True
+		else:
+			print(f"Please enter a valid choice ({choices_str}).")
