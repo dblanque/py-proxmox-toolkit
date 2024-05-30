83170433b1386dc553e1c04e00b34038b1cb8fbe
dblanque <dylan.blanque@gmail.com>
2024-05-30 19:03:49 -0300

    Add yes/no msg

--- py_pve_toolkit/utils/yes_no_input.py
+++ py_pve_toolkit/utils/yes_no_input.py
@@ -5,7 +5,9 @@ DEFAULT_CHOICES = {
 def yes_no_input(
 		msg: str,
 		input_default=None,
-		input_choices=DEFAULT_CHOICES
+		input_choices=DEFAULT_CHOICES,
+		yes_msg: str=None,
+		no_msg: str=None
 	):
 	choices_str = '|'.join(input_choices)
 	choices_str = f"({choices_str})"
@@ -15,12 +17,16 @@ def yes_no_input(
 	while True:
 		r = input(f"{msg} {choices_str}{default_str}:")
 		if r.lower() in input_choices["yes"]:
+			if yes_msg: print(yes_msg)
 			return True
 		elif r.lower() in input_choices["no"]:
+			if no_msg: print(no_msg)
 			return False
 		elif input_default == True or input_default in input_choices["yes"]:
+			if yes_msg: print(yes_msg)
 			return True
 		elif input_default == False or input_default in input_choices["no"]:
-			return True
+			if no_msg: print(no_msg)
+			return False
 		else:
 			print(f"Please enter a valid choice ({choices_str}).")
