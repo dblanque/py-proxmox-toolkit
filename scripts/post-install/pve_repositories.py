90c43e98167e234c121c063ea91110da35ea126c
dblanque <dylan.blanque@gmail.com>
2024-05-30 16:18:49 -0300

    Add PVE version checking

--- scripts/post-install/pve_repositories.py
+++ scripts/post-install/pve_repositories.py
@@ -9,3 +9,24 @@ if not VENV_DIR:
 	print(VENV_DIR)
 	raise Exception('Could not append VENV_DIR to PATH')
 sys.path.append(VENV_DIR)
+
+MIN_VERSION = "8.0.0"
+from py_pve_toolkit.debian import os_release
+from py_pve_toolkit.proxmox.pve_manager import pve_version_exists, get_pve_version
+from packaging.version import Version
+
+def main():
+	# Check if proxmox version valid (>8.0)
+	release_info = os_release.get_data()
+	if release_info["id"] != "debian": raise Exception()
+	if not pve_version_exists: raise Exception()
+	if Version(get_pve_version()) < Version(MIN_VERSION): raise Exception()
+
+	# Setting debian sources
+	# Setting PVE No-Subscription or Enterprise Sources
+	# Setting CEPH No-Subscription or Enterprise Sources
+	# Disabling HA (Default NO)
+	# Update Proxmox
+	# Offer Reboot
+
+if __name__ == "__main__": main()
