b49f2ce14bd91bdae9fd47a781eaed7684650962
dblanque <dylan.blanque@gmail.com>
2024-06-03 11:34:32 -0300

    Add extra_dirs arg to parser.

--- scripts/post_install/pve_backup.py
+++ scripts/post_install/pve_backup.py
@@ -18,20 +18,28 @@ def argparser(parser: argparse.ArgumentParser):
 	parser.prog = 'Proxmox VE Metadata Backup Script'
 	parser.description = 'This program is used to backup relevant Proxmox VE Host and Guest Configuration Metadata.'
 	parser.add_argument('-p', '--output-path', default="/opt", help="Backup Tar output path, use without trailing slash.")
+	parser.add_argument('-e', '--extra-dirs', help="Extra directories to backup.", nargs="+")
 	return parser
+
+def append_tar_dirs(list_to_check: list, result: list) -> list:
+	for d in list_to_check:
+		if not os.path.isdir(d):
+			print_c(bcolors.L_YELLOW, f"Directory ({d}) does not exist, skipping.")
+			continue
+		result.append(d)
+	return result
 
 def main(argv_a):
 	backup_date = datetime.now().astimezone(timezone.utc)
 	backup_date_fmted = backup_date.strftime(DATE_FMT)
 	tar_dirs = list()
+	if hasattr(argv_a, "extra_dirs"):
+		BKP_DIRS_EXTRA = argv_a.extra_dirs
 	if not os.path.isdir(argv_a.output_path):
 		try: os.mkdir(argv_a.output_path)
 		except: raise
-	for d in BKP_DIRS:
-		if not os.path.isdir(d):
-			print_c(bcolors.L_YELLOW, f"Directory ({d}) does not exist, skipping.")
-			continue
-		tar_dirs.append(d)
+	tar_dirs = append_tar_dirs(BKP_DIRS, tar_dirs)
+	tar_dirs = append_tar_dirs(BKP_DIRS_EXTRA, tar_dirs)
 	try:
 		timer_s = perf_counter()
 		cmd: list = f"tar -czvf {argv_a.output_path}/pve-bkp-{backup_date_fmted}.tar.gz --absolute-names".split() + tar_dirs
