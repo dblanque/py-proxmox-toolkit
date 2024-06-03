2d1bc0cc40b707534f08d0dea9d91a5920457c86
dblanque <dylan.blanque@gmail.com>
2024-06-03 10:49:49 -0300

    Add is_valid_version function.

--- core/debian/os_release.py
+++ core/debian/os_release.py
@@ -8,3 +8,9 @@ def get_data() -> dict:
 			os_data[key.lower()] = val
 		return os_data
 	except: raise
+
+def is_valid_version(os_release_info, min_version=12) -> bool:
+	major_version = int(os_release_info["version_id"].split(".")[0])
+	if os_release_info["id"] != "debian" or major_version < min_version:
+		return False
+	return True
