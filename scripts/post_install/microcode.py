41d7b27396c5b6f8814081dae418999fa8366b57
dblanque <dylan.blanque@gmail.com>
2024-06-03 10:12:51 -0300

    Add and update cpu microcode installer.

--- scripts/post_install/microcode.py
+++ scripts/post_install/microcode.py
@@ -2,6 +2,68 @@
 if __name__ == "__main__":
 	raise Exception("This python script cannot be executed individually, please use main.py")
 
-import signal
+import signal, subprocess, json, sys
+from core.format.colors import print_c, bcolors
 from core.signal_handlers.sigint import graceful_exit
 signal.signal(signal.SIGINT, graceful_exit)
+
+SUPPORTED_CPU_VENDORS = {
+	"authenticamd":{
+		"label":"AMD",
+		"deb":"amd64-microcode"
+	},
+	"genuineintel":{
+		"label":"Intel",
+		"deb":"intel-microcode"
+	}
+}
+
+def get_cpu_vendor():
+	lscpu = subprocess.Popen(['lscpu'], stdout=subprocess.PIPE)
+	grep = subprocess.Popen(["grep", "-oP", r'Vendor ID:\s*\K\S+'], stdin=lscpu.stdout, stdout=subprocess.PIPE)
+	output = subprocess.check_output("head -n 1".split(), stdin=grep.stdout)
+	lscpu.wait()
+	grep.wait()
+	return output.decode("utf-8").strip()
+
+def get_cpu_vendor_json():
+	output = subprocess.check_output("lscpu --json".split())
+	json_output: list = json.loads(output)['lscpu']
+	for d in json_output:
+		d: dict
+		f: str = d["field"]
+		f = f.lower().rstrip(":")
+		v = d["data"]
+		if "vendor" in f: return v
+
+def get_vendor_label(vendor: str) -> str:
+	return SUPPORTED_CPU_VENDORS[vendor.lower()]["label"]
+
+def get_vendor_microcode_name(vendor: str) -> str:
+	return SUPPORTED_CPU_VENDORS[vendor.lower()]["deb"]
+
+def main():
+	cpu_vendor = get_cpu_vendor()
+	if not cpu_vendor:
+		print_c(bcolors.L_RED, f"CPU Vendor not found.")
+		sys.exit(1)
+	if not cpu_vendor.lower() in SUPPORTED_CPU_VENDORS:
+		print_c(bcolors.L_YELLOW, f"CPU Vendor is not supported ({cpu_vendor}).")
+		sys.exit(1)
+	cpu_microcode_deb = get_vendor_microcode_name(cpu_vendor)
+	if not cpu_microcode_deb: 
+		print_c(bcolors.L_RED, "Could not get Microcode Package Name.")
+	print_c(bcolors.L_BLUE, f"Downloading and Installing {get_vendor_label(cpu_vendor)} Processor Microcode.")
+	apt_search = subprocess.check_output(
+		f"apt-cache search ^{cpu_microcode_deb}$".split()
+	)
+	if not apt_search.decode().strip().split(" - ")[0] == cpu_microcode_deb:
+		print_c(bcolors.L_RED, "Package not found, please add non-free-firmware APT Debian Repository.")
+		sys.exit(1)
+	try:
+		deb_available = subprocess.call(f"apt-cache search ^{cpu_microcode_deb}$")
+		subprocess.call("apt-get install -y".split()+[cpu_microcode_deb])
+	except:
+		raise
+	print_c(bcolors.L_GREEN, "Microcode Installed.")
+	sys.exit(0)
