447c8fe2baf3408bb850a642842ab0240a33b834
dblanque <dylan.blanque@gmail.com>
2024-06-03 10:49:40 -0300

    Change DEB Src format to dict.

--- scripts/post_install/apt_sources/debian.py
+++ scripts/post_install/apt_sources/debian.py
@@ -1,4 +1,3 @@
-
 SRC_DEB_BOOKWORM_SYNTAX = """# Debian Repository Sources
 deb http://deb.debian.org/debian {0} main
 deb-src http://deb.debian.org/debian {0} main
@@ -12,3 +11,7 @@ deb-src http://deb.debian.org/debian {0}-updates main
 deb http://ftp.us.debian.org/debian/ {0} main contrib non-free non-free-firmware
 deb http://security.debian.org/ {0}-security main contrib non-free non-free-firmware
 """
+
+DEB_LISTS = {
+	"bookworm": SRC_DEB_BOOKWORM_SYNTAX
+}
