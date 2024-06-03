c218776f1121377fb903612c4ad2ad282fe0b4df
dblanque <dylan.blanque@gmail.com>
2024-06-03 10:59:46 -0300

    Remove .us subdomain.

--- scripts/post_install/apt_sources/debian.py
+++ scripts/post_install/apt_sources/debian.py
@@ -8,7 +8,7 @@ deb-src http://deb.debian.org/debian-security/ {0}-security main
 deb http://deb.debian.org/debian {0}-updates main
 deb-src http://deb.debian.org/debian {0}-updates main
 
-deb http://ftp.us.debian.org/debian/ {0} main contrib non-free non-free-firmware
+deb http://ftp.debian.org/debian/ {0} main contrib non-free non-free-firmware
 deb http://security.debian.org/ {0}-security main contrib non-free non-free-firmware
 """
 
