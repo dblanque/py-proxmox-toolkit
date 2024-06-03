571884c1372df809f8cf433edc5391bbada716df
dblanque <dylan.blanque@gmail.com>
2024-06-03 10:49:21 -0300

    Add PBS Sources.

---
+++ scripts/post_install/apt_sources/pbs.py
@@ -0,0 +1,10 @@
+SRC_PBS_NO_SUBSCRIPTION = """
+# PBS pbs-no-subscription repository provided by proxmox.com
+# NOT recommended for production use
+deb http://download.proxmox.com/debian/pbs {0} pbs-no-subscription
+"""
+
+SRC_PBS_ENTERPRISE = """
+# PBS pbs-enterprise repository provided by proxmox.com
+# deb https://enterprise.proxmox.com/debian/pbs {0} pbs-enterprise
+"""
