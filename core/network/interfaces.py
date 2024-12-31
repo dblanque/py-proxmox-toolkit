eba676939564b85557e2558559d088e0b72cda2a
dblanque <dylan.blanque@gmail.com>
2024-12-30 22:41:36 -0300

    Improvements to network listing script.

--- core/network/interfaces.py
+++ core/network/interfaces.py
@@ -9,7 +9,15 @@ PHYSICAL_INTERFACE_PATTERNS=[
 	r"^ens[0-9].*$",
 ]
 
-def get_physical_interfaces(interface_patterns=None, override_patterns=False, verbose=False, exclude_patterns=None):
+VIRTUAL_INTERFACE_PATTERNS=[
+	r"^veth[0-9].*$",
+	r"^fwbr[0-9].*$",
+	r"^fwln[0-9].*$",
+	r"^fwpr[0-9].*$",
+	r"^tap[0-9a-fA-F].*$",
+]
+
+def get_interfaces(interface_patterns=None, override_patterns=False, verbose=False, exclude_patterns=None):
 	"""
 	Fetches physical interface names and returns them as a list.
 	If override_patterns is set to True then only the patterns passed will be matched.
@@ -17,7 +25,7 @@ def get_physical_interfaces(interface_patterns=None, override_patterns=False, ve
 	if not interface_patterns and override_patterns:
 		print("interface_patterns should contain at least one Regex Pattern if override_patterns is used.")
 
-	physical_interfaces=list()
+	filtered_interfaces=list()
 	network_interfaces=os.listdir('/sys/class/net/')
 	check_patterns: list
 	if not override_patterns: check_patterns = PHYSICAL_INTERFACE_PATTERNS
@@ -38,8 +46,8 @@ def get_physical_interfaces(interface_patterns=None, override_patterns=False, ve
 
 		for regex in check_patterns:
 			if re.match(regex, iface):
-				physical_interfaces.append(iface)
+				filtered_interfaces.append(iface)
 				match = True
 
-		if not match and verbose: print_c(bcolors.L_BLUE, f"[DEBUG] - {iface} is not a physical interface, skipping.")
-	return physical_interfaces
+		if not match and verbose: print_c(bcolors.L_BLUE, f"[DEBUG] - Skipping {iface} (did not match regex).")
+	return filtered_interfaces
--- scripts/post_install/pve_list_interfaces.py
+++ scripts/post_install/pve_list_interfaces.py
@@ -2,14 +2,16 @@ if __name__ == "__main__":
 	raise Exception("This python script cannot be executed individually, please use main.py")
 
 import argparse
-from core.network.interfaces import get_physical_interfaces
+from core.network.interfaces import get_interfaces, PHYSICAL_INTERFACE_PATTERNS, VIRTUAL_INTERFACE_PATTERNS
 from core.format.colors import bcolors, print_c
 
 def argparser():
 	parser = argparse.ArgumentParser(
-		prog="Proxmox VE VMBR Generation Script",
-		description="This program is used to generate a Network Interfaces file with new VMBRs."
+		prog="Proxmox VE Network Interface Listing Script",
+		description="This program is used to list network interfaces."
 	)
+	parser.add_argument("-p", "--physical", help="Physical Interface Filtering.", action="store_true")
+	parser.add_argument("-v", "--virtual", help="Virtual Interface Filtering.", action="store_true")
 	parser.add_argument("-e", "--exclude", help="Regexes to exclude from physical interface discovery.", nargs="+", default=None)
 	parser.add_argument("-r", "--regex", help="Regexes to select in physical interfaces discovery.", nargs="+", default=None)
 	parser.add_argument("-o", "--only-regex", help="Use only specified regexes to detect network interfaces.", action="store_true")
@@ -18,8 +20,19 @@ def argparser():
 
 def main(argv_a):
 	print_c(bcolors.L_YELLOW, "Scanning Physical Network Interfaces.")
-	interfaces = get_physical_interfaces(
-		interface_patterns=argv_a.regex,
+	regex_list = list()
+	if not argv_a.physical and not argv_a.virtual and not argv_a.regex:
+		regex_list = [ r"^.*$" ]
+
+	if argv_a.physical:
+		regex_list = regex_list + PHYSICAL_INTERFACE_PATTERNS
+	if argv_a.virtual:
+		regex_list = regex_list + VIRTUAL_INTERFACE_PATTERNS
+	if len(argv_a.regex) > 0:
+		regex_list = regex_list + argv_a.regex
+
+	interfaces = get_interfaces(
+		interface_patterns=regex_list,
 		override_patterns=argv_a.only_regex,
 		verbose=argv_a.verbose
 	)
