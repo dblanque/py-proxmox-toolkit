2119572597e54166a5c41b79886ae543e0990ec3
dblanque <dylan.blanque@gmail.com>
2025-01-23 18:20:33 -0300

    Improved main executor script to distinguish interactive mode.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import argparse, sys
+is_interactive = bool(getattr(sys, 'ps1', sys.flags.interactive))
+if is_interactive: print("Interactive mode enabled.")
 parser = argparse.ArgumentParser(
 	prog='Main Program file for Python Proxmox Toolkit Execution',
 	description='Use this program to execute sub-scripts from the toolkit',
@@ -7,16 +9,26 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('filename')
 args, unknown_args = parser.parse_known_args()
+
+# Construct sub-script name
 try:
 	parsed_filename = str(args.filename)
 	if parsed_filename.endswith(".py"):
 		parsed_filename = parsed_filename.split(".py")[0]
 		parsed_filename = parsed_filename.replace("/",".")
 except: raise
-script_func = getattr(__import__(parsed_filename, fromlist=["main"]), "main")
-script_parser = None
-if hasattr(__import__(parsed_filename, fromlist=["argparser"]), "argparser"):
-	script_parser = getattr(__import__(parsed_filename, fromlist=["argparser"]), "argparser")
+
+# Import main function from sub-script
+try:
+	script_func = getattr(__import__(parsed_filename, fromlist=["main"]), "main")
+	script_parser = None
+	if hasattr(__import__(parsed_filename, fromlist=["argparser"]), "argparser"):
+		script_parser = getattr(__import__(parsed_filename, fromlist=["argparser"]), "argparser")
+except:
+	if is_interactive: print("No main function detected.")
+	else: raise
+
+# Import argparser function from subscript if existent.
 try:
 	if script_parser:
 		new_parser: argparse.ArgumentParser = script_parser()
@@ -24,5 +36,9 @@ try:
 		args = new_parser.parse_args()
 		script_func(args)
 	else: script_func()
-except: raise
-sys.exit(0)
+except: 
+	if is_interactive: print("No argparser function detected.")
+	else: raise
+
+if not is_interactive:
+	sys.exit(0)
