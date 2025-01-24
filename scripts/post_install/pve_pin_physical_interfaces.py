d4e4eb3b2152d64256b7be2d4cebc4136dfd6aff
dblanque <dylan.blanque@gmail.com>
2025-01-24 15:41:44 -0300

    Added UDEV link write functionality

--- scripts/post_install/pve_pin_physical_interfaces.py
+++ scripts/post_install/pve_pin_physical_interfaces.py
@@ -7,7 +7,7 @@ from core.format.colors import bcolors, print_c
 from core.parser import make_parser, ArgumentParser
 from core.templates.udev.overrides import UDEV_BY_MAC_ADDRESS
 from core.validators.mac import mac_address_validator
-import subprocess
+import subprocess, os
 
 def argparser(**kwargs) -> ArgumentParser:
 	parser = make_parser(
@@ -15,10 +15,12 @@ def argparser(**kwargs) -> ArgumentParser:
 		description="This program is used to pin network interfaces to their corresponding MAC Address and/or Serial Number.",
 		**kwargs
 	)
+	parser.add_argument("-p", "--print", help="Print data instead of writing to UDEV Link Files.", action="store_true")
 	parser.add_argument("-v", "--verbose", action="store_true")
 	return parser
 
 def main(argv_a):
+	UDEV_PATH = "/etc/systemd/network"
 	print_c(bcolors.L_YELLOW, "Scanning Network Interfaces.")
 	regex_list = list(PHYSICAL_INTERFACE_PATTERNS)
 
@@ -28,6 +30,7 @@ def main(argv_a):
 	)
 	for iface_name in interfaces:
 		iface_mac_addr = None
+		udev_link_name = os.path.join(UDEV_PATH, f"10-{iface_name}.link")
 		mac_addr_subprocess = subprocess.Popen(f"cat /sys/class/net/{iface_name}/address".split(),
 			stdout=subprocess.PIPE, 
 			stderr=subprocess.PIPE,
@@ -38,4 +41,22 @@ def main(argv_a):
 			iface_mac_addr = out.decode("utf-8").strip()
 		if mac_address_validator(iface_mac_addr):
 			print(f"Interface {iface_name} will be pinned with MAC Address {iface_mac_addr}")
+			if os.path.isfile(udev_link_name):
+				print(f"UDEV Link File {udev_link_name} for Interface {iface_name} already exists, skipping.")
+				continue
+			with open(udev_link_name, "w") as iface_udev_link:
+				data = UDEV_BY_MAC_ADDRESS.format(
+					iface_name=iface_name,
+					iface_mac_addr=iface_mac_addr
+				).strip()
+				if argv_a.print:
+					print(f"Showing UDEV Link Template {udev_link_name} for Interface {iface_name}.")
+					print(data)
+				else:
+					print(f"Writing UDEV Link File {udev_link_name} for Interface {iface_name}.")
+					iface_udev_link.write(data)
+					print(f"UDEV Link Written.")
+
+		print_c(f"You may execute the command {bcolors.L_YELLOW}systemctl restart systemd-udev-trigger{bcolors.NC} to refresh the Interface UDEV Links.")
+
 
