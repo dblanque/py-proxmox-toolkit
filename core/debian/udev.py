c425aea8c3e69d9cd48b9c097d98af1db46534eb
dblanque <dylan.blanque@gmail.com>
2025-01-25 20:55:15 -0300

    Fix missing context manager usage.

--- core/debian/udev.py
+++ core/debian/udev.py
@@ -8,24 +8,24 @@ def get_inet_udev_info(device: str) -> dict:
 	:param str field: If specified only this field will be returned.
 	"""
 	data = {}
-	sp = subprocess.Popen(f"udevadm info /sys/class/net/{device}".split(),
+	with subprocess.Popen(f"udevadm info /sys/class/net/{device}".split(),
 		stdout=subprocess.PIPE, 
 		stderr=subprocess.PIPE,
-	)
-	out, err = sp.communicate()
-	returncode = sp.returncode
-	if returncode == 0:
-		out = out.decode("utf-8").split("\n")
-		for l in out:
-			if len(l.strip()) == 0: continue
-			udev_field, l_entry = l.split(": ", 1)
-			if "=" in l_entry:
-				l_field, l_value = l_entry.split("=", 1)
-			else:
-				l_field = None
-				l_value = l_entry
+	) as sp:
+		out, err = sp.communicate()
+		returncode = sp.returncode
+		if returncode == 0:
+			out = out.decode("utf-8").split("\n")
+			for line in out:
+				if len(line.strip()) == 0: continue
+				udev_field, l_entry = line.split(": ", 1)
+				if "=" in l_entry:
+					l_field, l_value = l_entry.split("=", 1)
+				else:
+					l_field = None
+					l_value = l_entry
 
-			if l_field:
-				data[l_field] = l_value
-			else: data[udev_field] = l_value
-	return data
+				if l_field:
+					data[l_field] = l_value
+				else: data[udev_field] = l_value
+		return data
