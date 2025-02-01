b9b2eae1a60ea105fd17bf847fa23a11165f97ac
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-01 20:12:35 -0300

    Change script logic order.

--- scripts/guests/change_id.py
+++ scripts/guests/change_id.py
@@ -219,7 +219,14 @@ def main(argv_a, **kwargs):
 		logger.error("Guest must be in stopped state (Currently %s)", guest_state)
 		sys.exit(ERR_GUEST_NOT_STOPPED)
 
-	# Add vmstate disks to configuration.
+	# Generate new config file path
+	old_cfg_path = guest_cfg_details['path']
+	new_cfg_path = guest_cfg_details['path'].replace(
+		f"{id_origin}.conf",
+		f"{id_target}.conf"
+	)
+
+	# Add snapshot vmstate disks to configuration.
 	if guest_snapshots:
 		for snapshot in guest_snapshots:
 			snapshot_cfg = parse_guest_cfg(
@@ -243,6 +250,7 @@ def main(argv_a, **kwargs):
 						logger.debug("Parsed Snapshot VM State: %s", parsed_disk)
 						guest_disks.append(parsed_disk)
 
+	# Second prompt if snapshots present
 	if not argv_a.yes and guest_snapshots:
 		print(f"Guest {id_origin} has {len(guest_snapshots)} snapshots that could be "+
 		"irreversibly affected if the process does not finish correctly.")
@@ -251,41 +259,15 @@ def main(argv_a, **kwargs):
 			print_c(bcolors.L_BLUE, NORMAL_PROMPT_EXIT_MSG)
 			sys.exit(0)
 
-	logger.info("The following disks will be renamed: ")
 	# For each discovered disk, do pre-checks
+	logger.info("The following disks will be renamed: ")
 	for key, value in guest_cfg.items():
 		if not is_valid_guest_disk_type(key, value): continue
 		parsed_disk = parse_guest_disk(disk_name=key, disk_values=value)
 		if parsed_disk:
 			guest_disks.append(parsed_disk)
 
-	# Change Config ID - Move config file
-	old_cfg_path = guest_cfg_details['path']
-	new_cfg_path = guest_cfg_details['path'].replace(
-		f"{id_origin}.conf",
-		f"{id_target}.conf"
-	)
-	args_mv = ["/usr/bin/mv", old_cfg_path, new_cfg_path]
-	if guest_on_remote_host:
-		args_mv = args_ssh + args_mv
-
-	# Rename Guest Configuration
-	if argv_a.dry_run:
-		logger.info(args_mv)
-	else:
-		with subprocess.Popen(args_mv, stdout=subprocess.PIPE) as proc:
-			proc_o, proc_e = proc.communicate()
-			if proc.returncode != 0:
-				raise Exception(
-					f"Bad command return code ({proc.returncode}).",
-					proc_o.decode(),
-					proc_e.decode()
-				)
-
-	replication_jobs = get_guest_replication_jobs(old_id=id_origin)
-	if replication_jobs:
-		logger.debug("Found replication targets: " + ", ".join(replication_jobs.keys()))
-
+	# Re-assign disks
 	for disk in guest_disks:
 		disk: DiskDict
 		d_storage = get_storage_cfg(disk["storage"])
@@ -303,8 +285,12 @@ def main(argv_a, **kwargs):
 			logger.exception(e)
 			pass
 
+	# Remove old Replication Jobs
+	replication_jobs = get_guest_replication_jobs(old_id=id_origin)
 	if replication_jobs:
-		# Remove old Replication Jobs
+		logger.debug("Found replication targets: " + ", ".join(replication_jobs.keys()))
+
+	if replication_jobs:
 		for job_name, job in replication_jobs.items():
 			job_name: str
 			job: ReplicationJobDict
@@ -339,6 +325,17 @@ def main(argv_a, **kwargs):
 			logger.debug(" ".join(new_job_cmd))
 			if not argv_a.dry_run:
 				subprocess.call(new_job_cmd)
+
+	# Rename Guest Config File
+	args_mv = ["/usr/bin/mv", old_cfg_path, new_cfg_path]
+	if guest_on_remote_host:
+		args_mv = args_ssh + args_mv
+
+	# Rename Guest Configuration
+	if argv_a.dry_run:
+		logger.info(args_mv)
+	else:
+		subprocess.call(args_mv, stdout=subprocess.PIPE)
 
 	# Alter Backup Jobs
 	# see https://forum.proxmox.com/threads/create-backup-jobs-using-a-shell-command.110845/
