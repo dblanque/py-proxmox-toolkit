f4e2eddd3801ddfc1226be833d0778a77bde106b
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-01 15:54:01 -0300

    Change input default.

--- scripts/guests/change_id.py
+++ scripts/guests/change_id.py
@@ -267,7 +267,7 @@ def main(argv_a, **kwargs):
 		if guest_snapshots:
 			print(f"Guest {id_origin} has {len(guest_snapshots)} snapshots that could be "+
 		 	"irreversibly affected if the process does not finish correctly.")
-			yes_no_input("Do you wish to continue?")
+			yes_no_input("Do you wish to continue?", input_default=False)
 
 	if argv_a.dry_run: logger.info("Executing in dry-run mode.")
 	guest_cfg_details = get_guest_cfg_path(guest_id=id_origin, get_as_dict=True)
