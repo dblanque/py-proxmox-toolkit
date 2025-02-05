647b69f4940880112bd24afa2db6c3fba72b7caf
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-05 16:22:18 -0300

    Remove unused import.

--- scripts/setup/debian/set_apt_cacher.py
+++ scripts/setup/debian/set_apt_cacher.py
@@ -13,7 +13,6 @@ from core.validators.ipaddress import validate_ip
 from core.validators.port import validate_port
 from core.signal_handlers.sigint import graceful_exit
 from core.format.colors import bcolors, print_c
-from core.utils.prompt import yes_no_input
 
 ERR_CACHER_ALREADY_SET = 1
 ERR_INVALID_CACHER_ADDRESS = 2
