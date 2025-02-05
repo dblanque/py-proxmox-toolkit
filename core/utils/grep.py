ebb9e8bfe90def9b2778b8c6b935203a87df0f97
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-05 14:21:05 -0300

    Add recursive grep function.

---
+++ core/utils/grep.py
@@ -0,0 +1,17 @@
+from os import walk, path
+from re import Pattern, compile
+
+def grep_r (pattern: Pattern, dir, return_files=False):
+	if not isinstance(pattern, Pattern):
+		compiled_pattern = compile(pattern)
+	for parent, dirs, files in walk(dir):
+		for base_name in files:
+			filename = path.join(parent, base_name)
+			if path.isfile(filename):
+				with open(filename) as f:
+					for line in f:
+						if compiled_pattern.search(line):
+							if return_files:
+								yield filename
+							else:
+								yield line
