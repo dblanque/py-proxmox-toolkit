e08ba6b7a15f7eec42b46ced28e34f72051686a2
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-05 14:20:58 -0300

    Add port validation function.

---
+++ core/validators/port.py
@@ -0,0 +1,12 @@
+
+def validate_port(value: int, raise_exception=False):
+	try:
+		port = int(value)
+		if 1 <= port <= 65535:
+			return True
+	except ValueError:
+		if not raise_exception:
+			return False
+		else:
+			raise
+	return False
