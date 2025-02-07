a13c4c6f734fd46dd572006dff32812bdeae4305
Dylan Blanque <dylan.blanque@gmail.com>
2025-02-07 02:04:45 -0300

    Change toolkit_path variable name to project_root in function get_allowed_paths.

--- core/utils/path.py
+++ core/utils/path.py
@@ -6,16 +6,16 @@ EXCLUDED_FILES = [
 	"template.py",
 ]
 
-def get_allowed_paths(toolkit_path: str, subdirectory: str = None):
-	if not os.path.isdir(toolkit_path):
-		raise ValueError(toolkit_path, "Must be a path.")
+def get_allowed_paths(project_root: str, subdirectory: str = None):
+	if not os.path.isdir(project_root):
+		raise ValueError(project_root, "Must be a path.")
 	if subdirectory:
 		if not os.path.isdir(subdirectory):
 			raise ValueError(subdirectory, "Must be a path.")
 	"""Precompute all valid script paths relative to toolkit directory"""
 	paths = []
 	for path in glob.glob(os.path.join(subdirectory, "**", "*.py"), recursive=True):
-		rel_path = os.path.relpath(path, toolkit_path)
+		rel_path = os.path.relpath(path, project_root)
 		basename = os.path.basename(rel_path)
 		if basename.startswith("__") or basename in EXCLUDED_FILES:
 			continue
