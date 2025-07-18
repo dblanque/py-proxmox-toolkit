59ec79190f8fcab9685e10a661e03cee6eb6da38
Dylan Blanque <dylan.blanque@gmail.com>
2025-07-18 01:49:50 -0300

    Minor fixes to get_all_allowed_paths fn.

--- core/utils/path.py
+++ core/utils/path.py
@@ -7,20 +7,23 @@ EXCLUDED_FILES = [
 ]
 
 
-def get_all_allowed_paths(project_root: str, subdirectory: str = None):
+def get_all_allowed_paths(project_root: str, subdirectory: str | None = None):
+	"""Precompute all valid script paths relative to toolkit directory"""
 	if not os.path.isdir(project_root):
 		raise ValueError(project_root, "Must be a path.")
-	if subdirectory:
-		if not os.path.isdir(subdirectory):
-			raise ValueError(subdirectory, "Must be a path.")
-	"""Precompute all valid script paths relative to toolkit directory"""
+	if subdirectory and not os.path.isdir(subdirectory):
+		raise ValueError(subdirectory, "Must be a path.")
 	paths = []
-	for _path in glob.glob(os.path.join(subdirectory, "**", "*.py"), recursive=True):
-		_rel_path = os.path.relpath(_path, project_root)
-		basename = os.path.basename(_rel_path)
-		if basename.startswith("__") or basename in EXCLUDED_FILES:
-			continue
-		paths.append(_rel_path)
+	if isinstance(subdirectory, str):
+		for _path in glob.glob(
+			os.path.join(subdirectory, "**", "*.py"),
+			recursive=True
+		):
+			_rel_path = os.path.relpath(_path, project_root)
+			basename = os.path.basename(_rel_path)
+			if basename.startswith("__") or basename in EXCLUDED_FILES:
+				continue
+			paths.append(_rel_path)
 	return paths
 
 
