0ea811985022dbaa142804468b64de60cf63e5c3
Dylan Blanque <dylan.blanque@gmail.com>
2025-07-28 19:47:33 -0300

    Add non-free suite tag.

--- scripts/setup/apt/sources/debian.py
+++ scripts/setup/apt/sources/debian.py
@@ -1,12 +1,12 @@
 SRC_DEB_BOOKWORM_SYNTAX = """# Debian Repository Sources
-deb https://deb.debian.org/debian {0} main non-free-firmware
-deb-src https://deb.debian.org/debian {0} main non-free-firmware
+deb https://deb.debian.org/debian {0} main non-free-firmware non-free
+deb-src https://deb.debian.org/debian {0} main non-free-firmware non-free
 
-deb https://security.debian.org/debian-security {0}-security main non-free-firmware
-deb-src https://security.debian.org/debian-security {0}-security main non-free-firmware
+deb https://security.debian.org/debian-security {0}-security main non-free-firmware non-free
+deb-src https://security.debian.org/debian-security {0}-security main non-free-firmware non-free
 
-deb https://deb.debian.org/debian {0}-updates main non-free-firmware
-deb-src https://deb.debian.org/debian {0}-updates main non-free-firmware
+deb https://deb.debian.org/debian {0}-updates main non-free-firmware non-free
+deb-src https://deb.debian.org/debian {0}-updates main non-free-firmware non-free
 """
 
 DEB_LISTS = {"bookworm": SRC_DEB_BOOKWORM_SYNTAX}
