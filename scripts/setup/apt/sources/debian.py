294fc9c33333b2a523691a54a4323f82951b90b1
Dylan Blanque <dylan.blanque@gmail.com>
2025-07-28 19:46:59 -0300

    Update Bookworm syntax for apt sources.

--- scripts/setup/apt/sources/debian.py
+++ scripts/setup/apt/sources/debian.py
@@ -1,15 +1,12 @@
 SRC_DEB_BOOKWORM_SYNTAX = """# Debian Repository Sources
-deb http://deb.debian.org/debian {0} main
-deb-src http://deb.debian.org/debian {0} main
+deb https://deb.debian.org/debian {0} main non-free-firmware
+deb-src https://deb.debian.org/debian {0} main non-free-firmware
 
-deb http://deb.debian.org/debian-security/ {0}-security main
-deb-src http://deb.debian.org/debian-security/ {0}-security main
+deb https://security.debian.org/debian-security {0}-security main non-free-firmware
+deb-src https://security.debian.org/debian-security {0}-security main non-free-firmware
 
-deb http://deb.debian.org/debian {0}-updates main
-deb-src http://deb.debian.org/debian {0}-updates main
-
-deb http://ftp.debian.org/debian/ {0} main contrib non-free non-free-firmware
-deb http://security.debian.org/ {0}-security main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian {0}-updates main non-free-firmware
+deb-src https://deb.debian.org/debian {0}-updates main non-free-firmware
 """
 
 DEB_LISTS = {"bookworm": SRC_DEB_BOOKWORM_SYNTAX}
