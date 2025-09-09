c83a69d82df087947c50f85303f2856f8f3df1dc
Dylan Blanque <dylan.blanque@gmail.com>
2025-09-09 14:03:55 -0300

    Add deb822 formats for PBS, PVE sources.

--- scripts/setup/apt/sources/pbs.py
+++ scripts/setup/apt/sources/pbs.py
@@ -1,10 +1,40 @@
-SRC_PBS_NO_SUBSCRIPTION = """
+SRC_PBS_NO_SUBSCRIPTION_LIST = """
 # PBS pbs-no-subscription repository provided by proxmox.com
 # NOT recommended for production use
 deb http://download.proxmox.com/debian/pbs {0} pbs-no-subscription
 """
 
-SRC_PBS_ENTERPRISE = """
+SRC_PBS_ENTERPRISE_LIST = """
 # PBS pbs-enterprise repository provided by proxmox.com
 # deb https://enterprise.proxmox.com/debian/pbs {0} pbs-enterprise
 """
+
+SRC_PBS_NO_SUBSCRIPTION_SOURCES = """
+# PBS pbs-no-subscription repository provided by proxmox.com
+# NOT recommended for production use
+Types: deb
+URIs: http://download.proxmox.com/debian/pbs
+Suites: {0}
+Components: pbs-no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+"""
+
+SRC_PBS_ENTERPRISE_SOURCES = """
+# PBS pbs-enterprise repository provided by proxmox.com
+Types: deb
+URIs: https://enterprise.proxmox.com/debian/pbs
+Suites: {0}
+Components: pbs-enterprise
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+"""
+
+SRC_PBS_APT_FORMAT_MAP = {
+	"bookworm": {
+		"no-subscription": SRC_PBS_NO_SUBSCRIPTION_LIST,
+		"enterprise": SRC_PBS_ENTERPRISE_LIST,
+	},
+	"trixie": {
+		"no-subscription": SRC_PBS_NO_SUBSCRIPTION_SOURCES,
+		"enterprise": SRC_PBS_ENTERPRISE_SOURCES,
+	},
+}
--- scripts/setup/apt/sources/pve.py
+++ scripts/setup/apt/sources/pve.py
@@ -1,10 +1,40 @@
-SRC_PVE_NO_SUBSCRIPTION = """
+SRC_PVE_NO_SUBSCRIPTION_LIST = """
 # PVE pve-no-subscription repository provided by proxmox.com
 # NOT recommended for production use
 deb http://download.proxmox.com/debian/pve {0} pve-no-subscription
 """
 
-SRC_PVE_ENTERPRISE = """
+SRC_PVE_ENTERPRISE_LIST = """
 # PVE pve-enterprise repository provided by proxmox.com
 deb https://enterprise.proxmox.com/debian/pve {0} pve-enterprise
 """
+
+SRC_PVE_NO_SUBSCRIPTION_SOURCES = """
+# PVE pve-no-subscription repository provided by proxmox.com
+# NOT recommended for production use
+Types: deb
+URIs: http://download.proxmox.com/debian/pve
+Suites: {0}
+Components: pve-no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+"""
+
+SRC_PVE_ENTERPRISE_SOURCES = """
+# PVE pve-enterprise repository provided by proxmox.com
+Types: deb
+URIs: https://enterprise.proxmox.com/debian/pve
+Suites: {0}
+Components: pve-enterprise
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+"""
+
+SRC_PVE_APT_FORMAT_MAP = {
+	"bookworm": {
+		"no-subscription": SRC_PVE_NO_SUBSCRIPTION_LIST,
+		"enterprise": SRC_PVE_ENTERPRISE_LIST,
+	},
+	"trixie": {
+		"no-subscription": SRC_PVE_NO_SUBSCRIPTION_SOURCES,
+		"enterprise": SRC_PVE_ENTERPRISE_SOURCES,
+	},
+}
