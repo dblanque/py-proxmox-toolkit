b1bf7f3d529779ae924e9c2ba934ddd7a918be0b
Dylan Blanque <dylan.blanque@gmail.com>
2025-09-09 14:03:33 -0300

    Add CEPH Squid version sources.

--- scripts/setup/apt/sources/ceph.py
+++ scripts/setup/apt/sources/ceph.py
@@ -16,13 +16,32 @@ SRC_CEPH_REEF_ENTERPRISE = """# CEPH Reef Enterprise Repository
 deb http://download.proxmox.com/debian/ceph-reef {0} enterprise
 """
 
+SRC_CEPH_SQUID_NO_SUBSCRIPTION = """# CEPH Reef No Subscription Repository
+Types: deb
+URIs: http://download.proxmox.com/debian/ceph-squid
+Suites: {0}
+Components: no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+"""
+SRC_CEPH_SQUID_ENTERPRISE = """# CEPH Reef Enterprise Repository
+Types: deb
+URIs: https://enterprise.proxmox.com/debian/ceph-squid
+Suites: {0}
+Components: enterprise
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+"""
+
 CEPH_SOURCES = {
 	"QUINCY": {
-		"NO_SUBSCRIPTION": SRC_CEPH_QUINCY_NO_SUBSCRIPTION,
-		"ENTERPRISE": SRC_CEPH_QUINCY_ENTERPRISE,
+		"no-subscription": SRC_CEPH_QUINCY_NO_SUBSCRIPTION,
+		"enterprise": SRC_CEPH_QUINCY_ENTERPRISE,
 	},
 	"REEF": {
-		"NO_SUBSCRIPTION": SRC_CEPH_REEF_NO_SUBSCRIPTION,
-		"ENTERPRISE": SRC_CEPH_REEF_ENTERPRISE,
+		"no-subscription": SRC_CEPH_REEF_NO_SUBSCRIPTION,
+		"enterprise": SRC_CEPH_REEF_ENTERPRISE,
+	},
+	"SQUID": {
+		"no-subscription": SRC_CEPH_SQUID_NO_SUBSCRIPTION,
+		"enterprise": SRC_CEPH_SQUID_ENTERPRISE,
 	},
 }
